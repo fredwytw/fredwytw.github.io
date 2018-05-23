d436db13f4cb3d43a57e1c0ffec20539818f72c5
ytwu <fredw.ytw@gmail.com>
2018-05-23 16:55:03 +0800

    Correct the web3_init.js

--- js/web3_init.js
+++ js/web3_init.js
@@ -1,10 +1,10 @@
-var Web3 = require('web3');
+// var Web3 = require('web3');
 // set the provider of web3
 if (typeof web3 !== 'undefined') {
     console.debug(web3.currentProvider);
     console.log("web3 provider setup ok");
-    alert("web3 provider setup ok");
+    window.alert("web3 provider setup ok");
     web3 = new Web3(web3.currentProvider);
 } else {
-    alert("No currentProvider for web3");
+    window.alert("No currentProvider for web3");
 }

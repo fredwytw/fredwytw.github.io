e0918edfdfd010c1e8780ac7bbcbed8331a75e8b
ytwu <fredw.ytw@gmail.com>
2018-05-23 16:34:30 +0800

    Use alert

--- js/web3_init.js
+++ js/web3_init.js
@@ -3,6 +3,7 @@ var Web3 = require('web3');
 if (typeof web3 !== 'undefined') {
     console.debug(web3.currentProvider);
     console.log("web3 provider setup ok");
+    alert("web3 provider setup ok");
     web3 = new Web3(web3.currentProvider);
 } else {
     alert("No currentProvider for web3");
